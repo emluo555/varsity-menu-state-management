60a13e5135b9ae5341540f1385dceadb0e004978
emluo555 <emilyluo555@gmail.com>
2022-12-03 14:49:13 -0500

    first commit

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const MenuItem = props => (
-  <button class="menu-item">
+const MenuItem = props => {
+  const[isClicked, setIsClicked] = useState(false);
+
+  const handleClick = event => {
+    if(!isClicked) {
+      props.addPurchasedItem({
+        name: props.name,
+        price: props.price,
+      })
+    } else {
+      props.removePurchasedItem(props.name);
+    }
+    
+    setIsClicked(!isClicked);
+
+  };
+
+  return (
+  <button className = {isClicked ? 'menu-item selected' : 'menu-item'} onClick = {handleClick} >
     <img src={props.picture} alt={props.name} />
     <dt>{props.name}</dt>
     <dd>{props.price}</dd>
   </button>
-)
+  )
+}
 export default MenuItem
